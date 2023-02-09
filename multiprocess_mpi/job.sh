1a3b95c269874eae64c5aaeb9cfcd2a7fcaadf78
Wataru Sugiura <flat35hd99@gmail.com>
2023-02-09 16:04:17 +0900

    Fix group number of procs number

--- multiprocess_mpi/job.sh
+++ multiprocess_mpi/job.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -eu
-#PBS -l select=1:ncpus=64:ompthreads=4:mpiprocs=16
+#PBS -l select=1:ncpus=16:ompthreads=1:mpiprocs=16
 #PBS -l walltime=12:00:00
 
 prefix=$PREFIX_BENCHMARK_HPC
 prefix_input=$prefix/input_data
-prefix_bench=$prefix/multiprocess_per_gpu
+prefix_bench=$prefix/multiprocess_mpi
 FAST_WORKING_DIR=/lwork/users/$USER/$PBS_JOBID
 PMEMD=pmemd.MPI
 
 # Input ----------
 
-n_times=5
-n_procs=16
+n_times=1
+n_procs=8
 
 # ----------------
 
@@ -24,16 +24,19 @@ cp $prefix_input/* .
 
 # Create groupfile
 for i in `seq 1 ${n_procs}`;do
-  sed -i "s/IPROC/${i}/g" $prefix/multiprocess_mpi/groupfile.ref >> groupfile
+  echo $i
+  sed "s/IPROC/${i}/g" $prefix/multiprocess_mpi/groupfile.ref >> groupfile
 done
+echo "" >> groupfile
 
 for i_time in `seq 1 ${n_times}`;do
   mkdir -p $prefix_bench/output/procs_${n_procs}/time_${i_time}
   start_time=`date +%s`
 
-  mpirun -np $n_procs \
+  mpirun -np $((($n_procs)*2)) \
     $PMEMD -O \
     -ng $n_procs \
+    -rem 0 \
     -groupfile groupfile
 
   cp mdout_* $prefix_bench/output/procs_${n_procs}/time_${i_time}/
